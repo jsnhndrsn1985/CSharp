ce6d755a0ddbed7b8a758cbc78660ad7cb2f6811
jsnhndrsn1985 <jsnhndrsn1985@gmail.com>
2019-11-13 20:31:17 -0500

    Completed Module 4
    Completed Module 4

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
   class Book
   {
-     private List<double> grades;
-     private string name;
+    private List<double> grades;
+    private string name;
 
     public Book(string name)
     {
@@ -16,6 +17,25 @@ namespace GradeBook
     public void AddGrade(double grade)
     {
       grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach (var number in grades)
+      {
+        highGrade = Math.Max(number, highGrade);
+        lowGrade = Math.Min(number, lowGrade);
+        result =+ number;
+      }
+
+      result /= grades.Count;
+      Console.WriteLine($"The average grade is {result:N3}");
+      Console.WriteLine($"The highest grade is {highGrade:N3}");
+      Console.WriteLine($"The lowest grade is {lowGrade:N3}");
     }
   }
 }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -12,24 +12,6 @@ namespace GradeBook
       book.AddGrade(88.4);
       book.AddGrade(90.5);
       book.ShowStatistics();
-
-      List<double> grades = new List<double>() { 3.7, 4.7, 6.5, 7.4 };
-      grades.Add(56.1);
-
-      var result = 0.0;
-      var highGrade = double.MinValue;
-      var lowGrade = double.MaxValue;
-      foreach (var number in grades)
-      {
-        highGrade = Math.Max(number, highGrade);
-        lowGrade = Math.Min(number, lowGrade);
-        result = +number;
-      }
-      result /= grades.Count;
-      Console.WriteLine($"The average grade is {result:N3}");
-      Console.WriteLine($"The highest grade is {highGrade:N3}");
-      Console.WriteLine($"The lowest grade is {lowGrade:N3}");
     }
   }
-
 }
