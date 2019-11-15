596ebe46cfa70b32350598b165b27673da888df9
jsnhndrsn1985 <jsnhndrsn1985@gmail.com>
2019-11-14 21:44:26 -0500

    Module 6 partial
    Module 6 partial

---
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+  public class TypeTests
+  {
+    [Fact]
+    public void Test1()
+    {
+      //arrange
+      var book1 = GetBook("Book 1");
+      var book2 = GetBook("Book 2");
+      //act
+
+      //assert
+      Assert.Equal("", book1.Name);
+      Assert.Equal("", book1.Name);
+    }
+
+    Book GetBook(string name)
+    {
+      return new Book(name);
+    }
+  }
+}
